0a2e7435e66906b8b4870d40fdbb43c55d5c16da
iyage <iyaghe.ofunrein@decagon.dev>
2021-09-06 23:39:11 +0100

    change  makeComments method parameter from Comments object to int userId, int postId,String content and editComment parameter from int userId, int postId,String content to (int id,String content) to accommodate for comment id only nad updated content, also change  findUserCommentOnAPost method return from comments object class to a List object

--- src/main/java/com/blog/restapi_blog/services/CommentsService.java
+++ src/main/java/com/blog/restapi_blog/services/CommentsService.java
@@ -5,11 +5,11 @@ import com.blog.restapi_blog.model.Comments;
 import java.util.List;
 
 public interface CommentsService {
-    Comments makeCommments(Comments comments);
-    Comments editComment(int userId, int postId,String content);
+    Comments makeCommments(int userId,int postId, String content);
+    Comments editComment(int id,String content);
     List<Comments>findAllUserComments(int userId);
     List<Comments>findAllpostComments(int postId);
-    Comments findUserCommentOnAPost(int postId,int userId);
+    List<Comments> findUserCommentOnAPost(int postId,int userId);
     Comments deleteComment(int id);
 
 
